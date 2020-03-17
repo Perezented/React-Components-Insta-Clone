41ba8ca82932b5dc0d9c2f8189aab9b5d136f138
Perezented <57721734+Perezented@users.noreply.github.com>
2020-03-17 14:30:09 -0500

    Added the search bar to the app. Looking at the post section now.

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,18 +1,22 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+import dummyData from "../../dummy-data";
+// import data
 
 const PostsPage = () => {
-  // set up state for your data
-  return (
-    <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
-    </div>
-  );
+    console.log(dummyData);
+    const [data] = useState(dummyData);
+    console.log(data);
+    // set up state for your data
+    return (
+        <div className="posts-container-wrapper">
+            {/* map through data here to return a Post and pass data as props to Post */}
+            {data.map(p => console.log(p))}
+        </div>
+    );
 };
 
 export default PostsPage;
-
